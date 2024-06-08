4a9ef9e6897f69f1e3bd36b257ddacbc8a1fa10c
Bohdana K <bohdana.dev@gmail.com>
2024-06-08 23:52:59 +0300

    add resources & user crud & auth

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+const cookieSession = require('cookie-session');
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+app.use(
+  cookieSession({
+  keys: ['1a2s3d4f5g']
+})),
+
+  (app as any).set('etag', false);
+  app.use((req, res, next) => {
+    res.removeHeader('x-powered-by');
+    res.removeHeader('date');
+    next();
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
+  await app.listen(3000);
+}
+bootstrap();
